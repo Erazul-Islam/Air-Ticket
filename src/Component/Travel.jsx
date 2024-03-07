160f3093e095ae9b3833375f5b21c27fcd655347
Erazul Islam <erazul89@gmail.com>
2024-03-07 10:35:36 +0600

    added

--- src/Component/Travel.jsx
+++ src/Component/Travel.jsx
@@ -1,3 +1,5 @@
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+
 const Travel = () => {
     return (
         <div className="mt-12">
@@ -6,8 +8,15 @@ const Travel = () => {
                 <figure><img className="w-full" src="https://i.ibb.co/p3F0LmD/Rectangle-18-1.png" alt="Shoes" /></figure>
                 <div className="card-body">
                     <p className="font-semibold text-2xl">Stopover Opportunity in Istanbul with Turkish Airlines</p>
-                    <div className="card-actions justify-center">
-                        
+                    <div className="card-actions justify-between">
+                        <p className="flex gap-1">
+                            <img className="w-9 h-9 rounded-full" src="https://i.ibb.co/qrDPhkq/Rectangle-33.png" alt="" />
+                            <p className="mt-1">Ys Stafen</p>
+                        </p>
+                        <p className="flex gap-1 font-bold">
+                            <p className="text-yellow-400">Read More</p>
+                            <p className="text-yellow-400 font-bold mt-1"><MdKeyboardDoubleArrowRight /></p>
+                        </p>
                     </div>
                 </div>
             </div>
